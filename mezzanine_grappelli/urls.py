216b38b0daebb280df6185bbe9fd8b2db574de05
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2014-01-29 10:14:20 +0100

    add filebrowser urls

--- mezzanine_grappelli/urls.py
+++ mezzanine_grappelli/urls.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, include
+from filebrowser.sites import site
 
 
 urlpatterns = patterns(
     "",
 
-    ("^grappelli/", include("grappelli.urls"))
+    ("^grappelli/", include("grappelli.urls")),
+    ("^media-library/", include(site.urls))
 )
